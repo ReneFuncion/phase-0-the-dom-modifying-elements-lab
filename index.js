92b62fc2733e8557f6b8d5c3d97043d78affeb86
Rene Funcion <rcfun959@gmail.com>
2023-04-09 11:49:37 -0700

    Completed phase-0-the-dom-modifying-elements-la

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector("main").remove();     // Test Passed
+
+const newHeader = document.createElement('h1');     //Test passed
+//document.body.append(newHeader);
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "Rene is the champion";
